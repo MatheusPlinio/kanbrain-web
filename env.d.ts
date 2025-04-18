d82f9b85b6023917150d37bd2e8801be9a9948fe
MatheusPlinio <mplinio13@gmail.com>
2025-04-18 20:16:54 +0000

    create nextauth provider

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJs {
+    interface ProcessEnv {
+        GOOGLE_CLIENT_ID: string;
+        GOOGLE_CLIENT_SECRET: string;
+        GITHUB_CLIENT_ID: string;
+        GITHUB_CLIENT_SECRET: string;
+        NEXTAUTH_SECRET: string;
+        NEXTAUTH_URL: string;
+    }
+}
+
+export {};
